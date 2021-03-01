4c10e82a7f378f92ca296d2dad3ac5a9708e8ed2
Michael Maricchiolo <maricchioloemme@gmail.com>
2021-03-02 00:35:27 +0100

    complete a grid template for card products

--- src/components/Main/Card/Card.js
+++ src/components/Main/Card/Card.js
@@ -8,22 +8,9 @@ function Card() {
           <span>Title</span>
           <img src='https://images.pexels.com/photos/4123897/pexels-photo-4123897.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260' alt=''></img>
           <span>99€</span>
+          <button>Read more</button>
         </li>
-        <li className='Card'>
-          <span>Title</span>
-          <img src='https://images.pexels.com/photos/4123897/pexels-photo-4123897.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260' alt=''></img>
-         <span>99€</span>
-        </li>
-        <li className='Card'>
-          <span>Title</span>
-          <img src='https://images.pexels.com/photos/4123897/pexels-photo-4123897.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260' alt=''></img>
-         <span>99€</span>
-        </li>
-        <li className='Card'>
-          <span>Title</span>
-          <img src='https://images.pexels.com/photos/4123897/pexels-photo-4123897.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260' alt=''></img>
-         <span>99€</span>
-        </li>
+        
       </ul>
     
   )
