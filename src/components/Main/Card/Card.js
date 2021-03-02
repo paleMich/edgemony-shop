09bf2fada8c9efbfbf3269fb4e678e7b1f8e0570
Michael Maricchiolo <maricchioloemme@gmail.com>
2021-03-02 17:40:04 +0100

    edit: add a style with grid to card

--- src/components/Main/Card/Card.js
+++ src/components/Main/Card/Card.js
@@ -4,8 +4,10 @@ function Card({products}) {
   return (
       products.map((product) => <li className='Card'>
         <span>{product.title}</span>
-        <img src={product.image} alt=''></img>
-        <span>{product.price}</span>
+        <div className='img-container'>
+          <img src={product.image} alt=''></img>
+        </div>
+        <span>{product.price}€</span>
         <button>Read more</button>
       </li>
       )
