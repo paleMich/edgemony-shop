78f1d2c40377a20c3b36c77bbe1acb3573219420
Michael Maricchiolo <maricchioloemme@gmail.com>
2021-03-02 23:40:54 +0100

    edit: add new modal view for single product

--- src/components/Main/Card/Card.js
+++ src/components/Main/Card/Card.js
@@ -1,6 +1,9 @@
+import { useState } from 'react'
+
 import './Card.css'
 
 function Card({products}) {
+  const [ modalIsOpen, setModalIsOpen ] = useState(false)
   return (
       products.map((product) => <li className='Card'>
         <span>{product.title}</span>
@@ -8,7 +11,19 @@ function Card({products}) {
           <img src={product.image} alt=''></img>
         </div>
         <span>{product.price}€</span>
-        <button>Read more</button>
+        <button type='button' onClick={() => setModalIsOpen(!modalIsOpen)}>View details</button>
+        <div className={`modal ${modalIsOpen ? 'modal--is-visible' : ''}`}>
+          <div className='modal-overlay'> 
+            <span type='button' onClick={() => setModalIsOpen(!modalIsOpen)} className='close-modal'>close</span>
+            products.filter((product) => product.title === product.title<div>
+            <span>{product.title}</span>
+            <div className='img-container'>
+              <img src={product.image} alt=''></img>
+            </div>
+            <span>{product.price}€</span>
+            </div> )
+          </div>
+        </div>
       </li>
       )
   )
