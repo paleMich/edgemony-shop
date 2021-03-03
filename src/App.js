4a5a001055605d524562c419b8ad8cbd51889730
Michael Maricchiolo <maricchioloemme@gmail.com>
2021-03-03 17:29:29 +0100

    edit: complete API calls with hooks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import Main from "./components/Main/Main";
 
 import "./App.css";
-
-const fakeProducts = require("./mocks/data/products.json");
 
 const data = {
   title: "Edgemony Shop",
@@ -14,15 +12,27 @@ const data = {
     "https://edgemony.com/wp-content/uploads/2020/03/cropped-Logo-edgemony_TeBIANCO-04.png",
   cover:
     "https://images.pexels.com/photos/4123897/pexels-photo-4123897.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-  products: fakeProducts,
 };
 
 function App() {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then((response) => response.json())
+      .then((products) => {
+        setProducts(products);
+      })
+      .catch(() => {
+        console.log('error')
+      });         
+  }, []);
+
   return (
     <>
       <Header logo={data.logo} />
       <Hero title={data.title} cover={data.cover} description={data.description} />
-      <Main products={data.products}/>
+      <Main products={products}/>
     </>
   )
 }
