d0bd59a9e4d4bc042a9171c2e30f411fff551d3f
Michael Maricchiolo <maricchioloemme@gmail.com>
2021-03-04 17:09:34 +0100

    feat: added a search-bar to filter products

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,13 +1,28 @@
+import {useState} from 'react'
+// import Search from './Search-product/Search'
+
 import Card from './Card/Card'
 
 import './Main.css'
 import './Card/Card.css'
 
 function Main({products}) {
+  const [search, setSearch] = useState('')
+
+  const filtProduct = products.filter(data => {
+    return data.title.toLowerCase().includes(search.toLowerCase())
+  })
+
   return (
     <div className='Main'>
+      {/* <Search setSearch={setSearch}/> */}
+      <div className='SearchBar'>
+        <span>Search</span>
+        <input type='text' placeholder='Search a product' onChange={(e) => setSearch(e.target.value)} className='Search'></input>
+      </div>
+
       <ul className='Card-wrapper'>{
-        products.map((product) => 
+        filtProduct.map((product) => 
           <Card products={product} key={product.id}/>
         )}
       </ul>
