4cedf02a2f2d488fde450d1fb892bc07e47c3ef7
Michael Maricchiolo <maricchioloemme@gmail.com>
2021-03-09 12:01:12 +0100

    edit: modified css

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,14 +4,15 @@ function Header({logo, cart}){
   const mapPrice = cart.map(product => parseFloat(product.price))
   
   const finalPrice = mapPrice.reduce((partialAdd, a) => partialAdd + a, 0)    // function add(accumulator, a) { return accumulator + a }
+  // oppure cart.reduce((partialAdd, a) => partialAdd + a.price, 0)
  
   return <header className='Header'>
     <img src={logo} alt="edgemony-shop-logo" />
     <div className='section-cart'>
-      <span className='price'>{finalPrice} €</span>
-      <div className='Cart'>
+    { !!cart.length && <span className='price'>{finalPrice.toFixed(2)} €</span> }
+      <div className='cart-icon'>
         <i className="fa fa-shopping-cart" aria-hidden="true"></i>
-        <span className='items-num'>{cart.length}</span>
+        { !!cart.length && <span className='items-num'>{cart.length}</span> }           {/*  !! uguale a Boolean(csrt.length) */}
       </div>
     </div>
   </header>
